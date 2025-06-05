f0de870d10e3c9c0887e85ecdb10e3114b7b3c0d
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-06-05 11:26:39 -0600

    Fixed error linking category to item

--- marketplace/item/models.py
+++ marketplace/item/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
     return self.name
   
 class Item(models.Model):
-    category = models.ForeignKey
+    category = models.ForeignKey(Category, related_name="items", on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=255)
     description = models.TextField(blank =True, null=True)
     price = models.FloatField()
