03a3c835cb287c3d2a29b591c82c26c077a23c73
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-06-05 11:21:43 -0600

    Added Item model, registered it to admin site and settings media setup

--- marketplace/item/models.py
+++ marketplace/item/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -10,3 +11,13 @@ class Category(models.Model):
   
   def __str__(self):
     return self.name
+  
+class Item(models.Model):
+    category = models.ForeignKey
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank =True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='items_images', blank = True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name="items", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add = True)
