4574918faecdd08eb60dc08398d78f69af3a9e8c
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-06-09 11:32:19 -0600

    Addedform to template, customized fields and saved user to database

--- marketplace/core/forms.py
+++ marketplace/core/forms.py
@@ -3,7 +3,24 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-class SignUpForm(UserCreationForm):
+class SignupForm(UserCreationForm):
   class Meta:
     model = User
     fields = ('username', 'email', 'password1', 'password2')
+
+  username = forms.CharField(widget=forms.TextInput(attrs={
+    'placeholder': 'Your username',
+    'class': 'w-full py-4 px-6 rounded-xl'
+  }))
+  email = forms.CharField(widget=forms.EmailInput(attrs={
+    'placeholder': 'Your email',
+    'class': 'w-full py-4 px-6 rounded-xl'
+  }))
+  password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+    'placeholder': 'Your password',
+    'class': 'w-full py-4 px-6 rounded-xl'
+  }))
+  password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+    'placeholder': 'Repeat password',
+    'class': 'w-full py-4 px-6 rounded-xl'
+  }))
