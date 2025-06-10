4f9eab6e368bb4825f5d281c4c95bcc31a10860b
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-06-10 11:57:19 -0600

    Implemented delete item functionality

--- marketplace/dashboard/views.py
+++ marketplace/dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 
@@ -11,3 +11,4 @@ def index(request):
     'items': items
   })
 # Create your views here.
+
