0b336c07ec7bde0d67ed0bde2b8ef4bfe910d8fc
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-06-10 11:44:42 -0600

    Created template for dashboard, added urls file

--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -22,5 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
-    path('items/', include('item.urls'))
+    path('items/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
