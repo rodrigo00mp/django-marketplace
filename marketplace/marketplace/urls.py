49e57a80351d247824b88eaea1107f36465dd46b
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-06-11 12:06:01 -0600

    Created conversation urls file and added it to main urls

--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
     path('items/', include('item.urls')),
-    path('dashboard/', include('dashboard.urls'))
+    path('dashboard/', include('dashboard.urls')),
+    path('inbox/', include('conversation.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
