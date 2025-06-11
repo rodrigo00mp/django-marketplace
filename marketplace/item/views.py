5fbfc7857c7ffac56ba16e43404fd5f79a2a3617
Rodrigo Montelongo <rodrigo00mp@gmail.com>
2025-06-11 10:10:46 -0600

    Addedbrowsing by name

--- marketplace/item/views.py
+++ marketplace/item/views.py
@@ -6,9 +6,15 @@ from .models import Item
 
 
 def items(request):
+  query = request.GET.get('query', '')
   items = Item.objects.filter(is_sold=False)
+
+  if query:
+    items = items.filter(name__icontains=query)
+
   return render(request, 'item/items.html', {
-    'items': items
+    'items': items,
+    'query': query
   })
 # Create your views here.
 def detail(request, pk):
